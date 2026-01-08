a2e007efc12479984463af3060bbc6ea98673554
v.oleynikov <vasily.oleynikov@flant.com>
2026-01-08 13:12:34 +0300

    Updates

--- ucs/init.sh
+++ ucs/init.sh
@@ -2,5 +2,5 @@
 
 /ucs/.pyenv/versions/ucs/bin/python3 manage.py makemigrations
 /ucs/.pyenv/versions/ucs/bin/python3 manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@dashboard.dashboard', 'qwerty')" | /ucs/.pyenv/versions/ucs/bin/python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@ucs.ucs', 'qwerty')" | /ucs/.pyenv/versions/ucs/bin/python3 manage.py shell
 /ucs/.pyenv/versions/ucs/bin/uwsgi --ini /ucs/uwsgi/ucs.main
